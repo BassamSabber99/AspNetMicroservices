1b57f1a1e4e70c2720f2ca7bfd88b2ecb2f23235
Bassam.Saber <Bassam.Saber@BASSAM-SABER>
2023-08-19 20:54:11 +0300

    add Catalog.Api service

---
+++ src/Services/Catalog/Catalog.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Api.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategoryName(string categoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
