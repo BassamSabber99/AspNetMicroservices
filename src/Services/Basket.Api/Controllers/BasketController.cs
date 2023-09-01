5c3a820a0abc34f811145c3a1198bc951b5b0833
Bassam.Saber <Bassam.Saber@BASSAM-SABER>
2023-09-01 22:12:33 +0300

    basket service developing

---
+++ src/Services/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using Basket.Api.Entities;
+using Basket.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _basketRepository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _basketRepository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepository.DeleteBasket(userName);
+            return Ok();
+        }
+
+    }
+}
---
+++ src/Services/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart() { }
+        public ShoppingCart(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public decimal totalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}
